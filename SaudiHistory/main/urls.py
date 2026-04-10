7ea998eba340b84b3ea8882442459729d7d48090
Rimas Alshahrani <remasmushbab@gmail.com>
2026-04-10 15:30:14 +0300

    Third version

--- SaudiHistory/main/urls.py
+++ SaudiHistory/main/urls.py
@@ -5,5 +5,19 @@ app_name="main"
 
 
 urlpatterns=[
-    path('', views.home_view, name="home_view")
+    path('', views.home_view, name="home_view"),
+    
+    path('king-abdulaziz/', views.king1_view, name="king1_view"),
+
+    path('king-saud/', views.king2_view, name="king2_view"),
+
+    path('king-faisal/', views.king3_view, name="king3_view"),
+    
+    path('king-khalid/', views.king4_view, name="king4_view"),
+    
+    path('king-fahd/', views.king5_view, name="king5_view"),
+    
+    path('king-abdullah/', views.king6_view, name="king6_view"),
+    
+    path('king-salman/', views.king7_view, name="king7_view"),
 ]
--- SaudiHistory/main/views.py
+++ SaudiHistory/main/views.py
@@ -4,5 +4,26 @@ from django.http import HttpRequest, HttpResponse
 
 def home_view(request:HttpRequest) :
     return render(request, 'main/home.html')
+
+
+def king1_view(request:HttpRequest) :
+    return render(request, 'main/king_abdulaziz.html')
+
+def king2_view(request:HttpRequest) :
+    return render(request, 'main/king_saud.html')
     
+def king3_view(request: HttpRequest):
+    return render(request, 'main/king_faisal.html')
+
+def king4_view(request: HttpRequest):
+    return render(request, 'main/king_khalid.html')
+
+def king5_view(request: HttpRequest):
+    return render(request, 'main/king_fahd.html')
+
+def king6_view(request: HttpRequest):
+    return render(request, 'main/king_abdullah.html')
+
+def king7_view(request: HttpRequest):
+    return render(request, 'main/king_salman.html')
 
